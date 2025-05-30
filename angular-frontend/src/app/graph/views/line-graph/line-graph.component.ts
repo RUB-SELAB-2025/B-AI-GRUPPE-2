3ab10000ac0b2b057d124c800fd29bd2fd154356
alexanderjkslfj <117545308+alexanderjkslfj@users.noreply.github.com>
2025-05-30 15:55:53 +0200

    align channel color with backend

--- angular-frontend/src/app/graph/views/line-graph/line-graph.component.ts
+++ angular-frontend/src/app/graph/views/line-graph/line-graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, ElementRef, inject, signal, viewChild, WritableSignal } from '@angular/core';
+import { Component, ElementRef, inject, Signal, signal, viewChild, WritableSignal } from '@angular/core';
 import * as d3 from 'd3';
 import { ResizeObserverDirective } from '../../../shared/resize-observer.directive';
 import { Channel, DataServer, SessionData } from '../../../omnai-datasource/data-server';
@@ -33,14 +33,13 @@ type Data = ChannelData[]
  * Stores and processes color and scale.
  */
 class ChannelView {
-  public readonly $hue = signal(0)
-  public readonly $color = computed(() => `hsl(${this.$hue()}, 100%, 50%)`)
+  public readonly $color
 
   public readonly targetScale: { min: number, max: number } = { min: 0, max: 0 }
   public readonly viewedScale: { min: number, max: number } = { min: 0, max: 0 }
 
-  constructor(hue: number) {
-    this.$hue.set(hue)
+  constructor(color: Signal<string>) {
+    this.$color = color
   }
 
   /**
@@ -104,41 +103,6 @@ export class LineGraphComponent {
   }
 
   /**
-   * Gets a hue that is as far away from preexisting hues as possible.
-   */
-  private getNewChannelHue(): number {
-    const channels = Object.values(this.channels)
-
-    // for the first channel, pick a random color
-    if (channels.length === 0) {
-      return Math.floor(Math.random() * 360)
-    }
-
-    const hues = channels.map(channel => channel.$hue())
-    hues.sort()
-
-    // check the spaces at the outer bounds
-    if (hues[0] > (360 - hues[hues.length - 1])) {
-      var biggestDiff = hues[0]
-      var biggestDiffHue = Math.floor(hues[0] / 2)
-    } else {
-      var biggestDiff = 360 - hues[hues.length - 1]
-      var biggestDiffHue = Math.floor(360 - biggestDiff / 2)
-    }
-
-    // check the spaces between the existing hues
-    for (let i = 1; i < hues.length; i++) {
-      const diff = hues[i] - hues[i - 1]
-      if (diff > biggestDiff) {
-        biggestDiff = diff
-        biggestDiffHue = Math.floor(hues[i - 1] + diff / 2)
-      }
-    }
-
-    return biggestDiffHue
-  }
-
-  /**
    * Updates the recorded dimensions of the svg.
    *
    * Gets called from an HTML callback.
@@ -151,7 +115,7 @@ export class LineGraphComponent {
   /**
    * Gets the currently viewed time frame.
    */
-  private getViewTime(): { start: number, end: number } {
+  public getViewTime(): { start: number, end: number } {
     const end = (this.viewedTime.end === null || this.viewedTime.end === -1)
       ? Date.now()
       : this.viewedTime.end
@@ -343,8 +307,7 @@ export class LineGraphComponent {
   private addNewChannels(data: Data) {
     for (const channelData of data) {
       if (!(channelData.channel.id in this.channels)) {
-        const hue = this.getNewChannelHue()
-        this.channels[channelData.channel.id] = new ChannelView(hue)
+        this.channels[channelData.channel.id] = new ChannelView(channelData.channel.color)
       }
     }
   }
