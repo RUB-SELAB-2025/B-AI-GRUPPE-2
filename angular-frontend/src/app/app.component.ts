2b7f99f320c9490d776769bba0edd4612c3064dd
Dennis <acceleratormainplan@gmail.com>
2025-06-24 15:15:05 +0200

    User instructions

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'OmnAIView';
-  
 }
